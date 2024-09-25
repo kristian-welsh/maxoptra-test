8ce55bfe4e81054ce3b76410dbb4f39c8165ac62
kristian welsh <kriswelsh@gmail.com>
2024-09-25 18:01:19 +0100

    fix up the starting template

--- src/test/java/com/maxoptra/kwelsh/HelloControllerTest.java
+++ src/test/java/com/maxoptra/kwelsh/HelloControllerTest.java
@@ -1,47 +1,42 @@
 package com.maxoptra.kwelsh;
 
 import com.jayway.restassured.RestAssured;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.is;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = App.class)
-@WebAppConfiguration
-@IntegrationTest({"server.port:0",
-        "spring.datasource.url:jdbc:h2:mem:interview-challenge-1;DB_CLOSE_ON_EXIT=FALSE"})
+@SpringBootTest(classes = App.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
 public class HelloControllerTest {
     @Value("${local.server.port}")
     int port;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeEach
+    public void setUp() {
         RestAssured.port = port;
     }
 
     @Test
-    public void testHello() throws Exception {
+    public void testHello() {
         when().get("/").then()
                 .body(is("Hello World!"));
     }
 
     @Test
-    public void testCalc() throws Exception {
+    public void testCalc() {
         given().param("left", 100)
                 .param("right", 200)
                 .get("/calc")
                 .then()
-                .body("left", is(100))
-                .body("right", is(200))
-                .body("answer", is(300));
+                .body(is("300"));
     }
 }
