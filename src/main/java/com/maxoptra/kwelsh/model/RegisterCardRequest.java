a71bf047aec3c39c86b7145dfdf90c316d6f50d9
kristian welsh <kriswelsh@gmail.com>
2024-09-26 18:15:18 +0100

    add single card entry form. do not use form submition action as it limits REST API design and future CSS visual design

---
+++ src/main/java/com/maxoptra/kwelsh/model/RegisterCardRequest.java
@@ -0,0 +1,12 @@
+package com.maxoptra.kwelsh.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RegisterCardRequest {
+    private String bankName;
+    private String cardNumber;
+    private String expiryDate;
+}
